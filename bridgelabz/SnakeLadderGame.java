7e8bbd22a5e66964be4c5dfc172cfd5bbf2a986e
Ganesh <ganeshgavhad9@gmail.com>
2021-07-04 12:20:50 +0530

    Added : Added function of roll dice

--- bridgelabz/SnakeLadderGame.java
+++ bridgelabz/SnakeLadderGame.java
@@ -1,10 +1,19 @@
 package bridgelabz;
 
+import java.util.Random;
+
 public class SnakeLadderGame {
+	
+	public static int rollDice() {
+		Random random=new Random();
+    	int ran=1+random.nextInt(6);
+    	return ran;
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Snack And Ladder Game Programs");
 		int playerPosition=0;
+		rollDice();
 	}
 
 }
